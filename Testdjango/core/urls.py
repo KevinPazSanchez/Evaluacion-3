85d0a98446d6d34394e0971490dedee1a5d2075f
Jose Huircaman <gonzalopainepan@gmail.com>
2021-06-28 02:10:00 -0400

    Se sube archivo urls.py, donde se agregan las funcionalidades

---
+++ Testdjango/core/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from core.views import home,agregar_proveedores, proveedores,eliminar_proveedores,modificar_proveedores,mensaje_modificar
+
+app_name='core'
+
+urlpatterns = [
+    path('', home, name="home"),
+    path('agregar/', agregar_proveedores , name="agregar"),
+    path('proveedores/', proveedores , name="proveedores"),
+    path('eliminar/<id>', eliminar_proveedores , name="eliminar"),
+    path('modificar/<id>', modificar_proveedores , name="modificar"),
+    path('mensaje/', mensaje_modificar, name="mensaje"),
+
+]
