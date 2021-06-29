fdad4eb2ec661dc3df712173ef01a63a2c291459
Jose Huircaman <gonzalopainepan@gmail.com>
2021-06-28 23:35:02 -0400

    Se agrega modelo de Servicio en archivo models.py

--- Testdjango/core/models.py
+++ Testdjango/core/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+class Servicio (models.Model):
+    id_servicio= models.IntegerField(primary_key=True, verbose_name='ID de servicio')
+    nombre_servicio = models.CharField(max_length=50, verbose_name='Nombre del Servicio')
+
+    def __str__(self):
+        return self.nombre_servicio
+
 class Proveedor(models.Model):
     rut=models.CharField(max_length=20)
     razon_social=models.CharField(max_length=50)
