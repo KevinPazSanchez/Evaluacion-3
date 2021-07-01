5ed7a2a330a2177b378c961fa336c803d21cf3db
Jose Huircaman <gonzalopainepan@gmail.com>
2021-06-30 21:57:01 -0400

    Se agrega metodo agregarproveedor desde la api rest

--- Testdjango/rest_proveedor/views.py
+++ Testdjango/rest_proveedor/views.py
@@ -16,5 +16,18 @@ def lista_proveedores(request):
     serializer = ProveedorSerializer(proveedor, many=True)
     return Response(serializer.data)
 
+@csrf_exempt
+@api_view(['POST'])
+def agregar_proveedor(request):
+    if request.method == 'POST':
+        data = JSONParser().parsers(request)
+        serializer = ProveedorSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
 
 # Create your views here.
