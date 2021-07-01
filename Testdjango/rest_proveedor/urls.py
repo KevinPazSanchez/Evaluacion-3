cdefc42423f15d9282c070193eb4c82cebb3cd00
Jose Huircaman <gonzalopainepan@gmail.com>
2021-06-30 22:36:58 -0400

    Se agrega path de detalle proveedor en urls.py

--- Testdjango/rest_proveedor/urls.py
+++ Testdjango/rest_proveedor/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path 
-from rest_proveedor.views import lista_proveedores, agregar_proveedor
+from rest_proveedor.views import lista_proveedores, agregar_proveedor, detalle_proveedor
 
 urlpatterns = [
     path('lista_proveedores', lista_proveedores, name='lista_proveedores'),
     path('agregar_proveedor', agregar_proveedor, name='agregar_proveedor' ),
+    path('detalle_proveedor/<id>', detalle_proveedor, name='detalle_proveedor'),
 ]
