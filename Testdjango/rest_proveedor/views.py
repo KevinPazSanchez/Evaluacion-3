01de80a55c9d09982a93f1b80b2bb91554442bf4
Jose Huircaman <gonzalopainepan@gmail.com>
2021-06-30 22:38:02 -0400

    Se agrega metodo get, put y delete para la api rest_proveedor

--- Testdjango/rest_proveedor/views.py
+++ Testdjango/rest_proveedor/views.py
@@ -26,8 +26,30 @@ def agregar_proveedor(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-
+        
+@api_view(['GET','PUT','DELETE'])
+def detalle_proveedor(request,id):
+    """
+    Get, update, o delete de un proveedor en particular
+    """
+    try:
+        proveedor = Proveedor.objects.get(rut=id)
+    except Proveedor.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = ProveedorSerializer(proveedor)
+        return Response(serializer.data)
+    
+    if request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = ProveedorSerializer(proveedor,data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        proveedor.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 # Create your views here.
