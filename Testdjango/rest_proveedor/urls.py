2c50ffbfd34808c6cfdf956fad4b0bbbb1e27e84
Jose Huircaman <gonzalopainepan@gmail.com>
2021-06-30 21:55:05 -0400

    Se agrega path agregar proveedor en archivo urls.py

--- Testdjango/rest_proveedor/urls.py
+++ Testdjango/rest_proveedor/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path 
-from rest_proveedor.views import lista_proveedores
+from rest_proveedor.views import lista_proveedores, agregar_proveedor
 
 urlpatterns = [
     path('lista_proveedores', lista_proveedores, name='lista_proveedores'),
+    path('agregar_proveedor', agregar_proveedor, name='agregar_proveedor' ),
 ]
